349b6f34069377ce24fa96ebfd1cc67fe50782df
yoshihiko-a <aoshi2025s@gmail.com>
2024-05-15 11:25:16 +0900

    ex00 ok

---
+++ ex01/vars.js
@@ -0,0 +1,23 @@
+// ************************************************************************** //
+//                                                                            //
+//                                                        :::      ::::::::   //
+//   vars.js                                            :+:      :+:    :+:   //
+//                                                    +:+ +:+         +:+     //
+//   By: yoaoki <yoaoki@student.42tokyo.jp>         +#+  +:+       +#+        //
+//                                                +#+#+#+#+#+   +#+           //
+//   Created: 2024/05/15 10:59:48 by yoaoki            #+#    #+#             //
+//   Updated: 2024/05/15 11:24:19 by yoaoki           ###   ########.fr       //
+//                                                                            //
+// ************************************************************************** //
+
+console.log("42 is a " + typeof("42"));
+console.log(42 + " is a " + typeof(42));
+
+/*
+42 is a string.
+42 is a number.
+42 is an object.
+[object Object] is an object.
+true is a boolean.
+undefined is an undefined.
+*/
